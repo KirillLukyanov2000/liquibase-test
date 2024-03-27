feeaa63b8008c7eba99c7de4540b6b76f7694b96
Kirill Lukyanov <kirs2000@mail.ru>
2024-03-27 13:19:20 +0300

    XML Liquibase Config added

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,8 +1,6 @@
 
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
-S
-#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
 Z
 *src/main/java/org/example/DbValidator.java,0\7\07e33a40354f1a8c21092ef3e181fde7946f1dc1
 ]
--- src/main/java/org/example/DbValidator.java
+++ src/main/java/org/example/DbValidator.java
@@ -14,6 +14,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DbValidator {
+    public static final String XML_CHANGELOG = "liquibase/xml_example/changelog.xml";
+    public static final String YAML_CHANGELOG = "liquibase/yaml_example/changelog.yaml";
+
     public static void main(String[] args) throws Exception {
 
         Map<String, Object> config = new HashMap<>();
@@ -28,7 +31,7 @@ public class DbValidator {
                         .getInstance()
                         .findCorrectDatabaseImplementation(new JdbcConnection(connection));
 
-                Liquibase liquibase = new liquibase.Liquibase("liquibase/changelog.yaml", new ClassLoaderResourceAccessor(), database);
+                Liquibase liquibase = new liquibase.Liquibase(YAML_CHANGELOG, new ClassLoaderResourceAccessor(), database);
 
                 liquibase.update(new Contexts(), new LabelExpression());
 
